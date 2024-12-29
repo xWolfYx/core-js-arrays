0a2b0c3a6709673bd006b00a0db6555650028306
xWolfYx <david.skliarov@gmail.com>
2024-12-29 16:01:31 +0400

    feat: resolve findAllOccurrences function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -71,8 +71,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((value) => value === item).length;
 }
 
 /**
