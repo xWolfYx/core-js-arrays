5f97dedc39992a5b44f73a87874c58de55fb5b48
xWolfYx <david.skliarov@gmail.com>
2025-01-16 18:26:31 +0400

    feat: resolve createNDimensionalArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -262,8 +262,13 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  // base case
+  if (n === 1) {
+    return new Array(size).fill(0);
+  }
+  // recursive function
+  return new Array(size).fill().map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
